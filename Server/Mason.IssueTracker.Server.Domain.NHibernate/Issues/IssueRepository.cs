4f9f7a6f672432d4d35bacdebe6d15ae57f18bee
JornWildt <jw@fjeldgruppen.dk>
2014-01-25 00:48:27 +0100

    Implementing cascading delete

--- Server/Mason.IssueTracker.Server.Domain.NHibernate/Issues/IssueRepository.cs
+++ Server/Mason.IssueTracker.Server.Domain.NHibernate/Issues/IssueRepository.cs
@@ -1,4 +1,5 @@
-﻿using Mason.IssueTracker.Server.Domain.Issues;
+﻿using Mason.IssueTracker.Server.Domain.Attachments;
+using Mason.IssueTracker.Server.Domain.Issues;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,13 @@ namespace Mason.IssueTracker.Server.Domain.NHibernate.Issues
 {
   public class IssueRepository : NHibernateGenericRepository<Issue, int>, IIssueRepository
   {
+    #region Dependencis
+
+    public IAttachmentRepository AttachmentRepository { get; set; }
+
+    #endregion
+
+
     #region IIssueRepository Members
 
     public List<Issue> IssuesForProject(int projectId)
@@ -20,6 +28,14 @@ namespace Mason.IssueTracker.Server.Domain.NHibernate.Issues
     public List<Issue> FindIssues(IssueSearchArgs args)
     {
       throw new NotImplementedException();
+    }
+
+
+    public override void Delete(Issue issue)
+    {
+      foreach (Attachment a in AttachmentRepository.AttachmentsForIssue(issue.Id))
+        AttachmentRepository.Delete(a);
+      base.Delete(issue);
     }
 
     #endregion
--- Server/Mason.IssueTracker.Server.Domain.NHibernate/Projects/ProjectRepository.cs
+++ Server/Mason.IssueTracker.Server.Domain.NHibernate/Projects/ProjectRepository.cs
@@ -1,9 +1,23 @@
-﻿using Mason.IssueTracker.Server.Domain.Projects;
+﻿using Mason.IssueTracker.Server.Domain.Issues;
+using Mason.IssueTracker.Server.Domain.Projects;
 
 
 namespace Mason.IssueTracker.Server.Domain.NHibernate.Projects
 {
   public class ProjectRepository : NHibernateGenericRepository<Project, int>, IProjectRepository
   {
+    #region Dependencies
+
+    public IIssueRepository IssueRepository { get; set; }
+
+    #endregion
+
+
+    public override void Delete(Project project)
+    {
+      foreach (Issue i in IssueRepository.IssuesForProject(project.Id))
+        IssueRepository.Delete(i);
+      base.Delete(project);
+    }
   }
 }
