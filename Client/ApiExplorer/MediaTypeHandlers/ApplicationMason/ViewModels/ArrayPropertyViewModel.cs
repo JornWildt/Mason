2cb37512d94903900b449c5ae6caedb36246d095
JornWildt <jw@fjeldgruppen.dk>
2014-11-29 11:16:28 +0100

    Show array item count. Remove NuGet stuff.

--- Client/ApiExplorer/MediaTypeHandlers/ApplicationMason/ViewModels/ArrayPropertyViewModel.cs
+++ Client/ApiExplorer/MediaTypeHandlers/ApplicationMason/ViewModels/ArrayPropertyViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using ApiExplorer.ViewModels;
 using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
 
 
 namespace ApiExplorer.MediaTypeHandlers.ApplicationMason.ViewModels
 {
   public class ArrayPropertyViewModel : PropertyViewModel
   {
+    public int Count
+    {
+      get { return ((ObservableCollection<ViewModel>)Value).Count; }
+    }
+
+
     public ArrayPropertyViewModel(ViewModel parent, JToken json, string name, object value)
       : base(parent, json, name, value)
     {
